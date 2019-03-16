9e65ab90d8d5c0919df1e17a091fff43aebfcea6
Sangyong Bae <kiros33@gmail.com>
2019-03-17 02:49:17 +0900

    1) fb2-1 프로젝트를 복사 2) cmake로 변환 3) main() 을 main.cpp/main.h 로 옮김. 4) arguments 처리로 -d, -f textfile 처리

---
+++ fb9-1/main.h
@@ -0,0 +1,32 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__ 1
+
+/*
+  Example 3-5. Advanced calculator header fb3-2.h
+  Declarations for a calculator fb3-1
+*/
+
+#ifdef _MSC_VER
+  /* strdup 경고 없애기 */
+  #define strdup _strdup
+#endif
+
+#ifdef _MSC_VER
+  /* fopen 경고 없애기 */
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+//#define YYDEBUG 1
+
+extern bool debug_flag;
+extern bool l_debug_flag;
+extern bool y_debug_flag;
+
+/* interface to the lexer */
+extern int yylineno; /* from lexer */
+extern char *yytext; /* from lexer */
+void yyerror(const char *s, ...);
+
+extern int yydebug;
+
+#endif
